c35ee56d2c06e66abf79561c16b206f7b5b4068f
Henne <henne90gen@gmail.com>
2018-05-25 09:51:48 +0200

    Introducing travis to build and deploy jars automatically

--- src/main/java/de/henne90gen/chestcounter/ChestCounter.java
+++ src/main/java/de/henne90gen/chestcounter/ChestCounter.java
@@ -18,7 +18,7 @@ public class ChestCounter implements IChestCounter {
 
 	public static final String MODID = "chestcounter";
 	public static final String NAME = "Chest Counter";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "@VERSION@";
 
 	private Logger logger;
 
